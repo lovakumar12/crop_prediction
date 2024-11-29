835e8b0c4d6f24f39468681a8aece0324adb32c4
lovakumar12 <kumarlova078@email.com>
2024-11-29 23:33:33 +0530

    second

--- src/init.py
+++ src/init.py
@@ -1,8 +1,14 @@
 # Import necessary libraries
 import pandas as pd
 import numpy as np
+import sys
+import os
+import pandas as pd
 
+# Use a relative path
+file_path = os.path.join(os.path.dirname(__file__), "../data/agriculture_dataset.csv")
+data = pd.read_csv(file_path)
 
 
 # Load the data
-data = pd.read_csv(r"C:\Users\kumar\rosh_corpararate\Machine learning\crop prediction\data\agriculture_dataset.csv")
+#data = pd.read_csv(r"C:\Users\kumar\rosh_corpararate\Machine learning\crop prediction\data\agriculture_dataset.csv")
